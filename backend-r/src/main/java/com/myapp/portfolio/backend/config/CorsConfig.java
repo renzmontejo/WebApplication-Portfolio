c32e32aeb266bc28cde4e4a7d03c8dc54d6747e0
rens <renzmontejo17@example.com>
2026-04-22 15:18:32 +0800

    add everything

--- backend-r/src/main/java/com/myapp/portfolio/backend/config/CorsConfig.java
+++ backend-r/src/main/java/com/myapp/portfolio/backend/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of("http://localhost:4200",
-                "https://web-application-portfolio.vercel.app"));
+                "https://web-application-portfolio.vercel.app",
+                "https://renzmontejo.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
