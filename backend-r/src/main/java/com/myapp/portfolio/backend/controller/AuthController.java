e447af577517449d0ed010a0f1fc84e8259df1f5
rens <renzmontejo17@example.com>
2026-04-22 16:46:29 +0800

    fix the AuthController

--- backend-r/src/main/java/com/myapp/portfolio/backend/controller/AuthController.java
+++ backend-r/src/main/java/com/myapp/portfolio/backend/controller/AuthController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/auth")
-@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
